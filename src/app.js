e574724218c320bba37685a3547cdd53089df5a9
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-05 23:52:39 +0000

    cleaned up imports

--- src/app.js
+++ src/app.js
@@ -1,23 +1,9 @@
 // load stylesheet
 import '../assets/app.scss';
 
-// load app libraries
-import path from 'path';
-import FileManager from './object/FileManager';
-
 // setup react
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, window.document.getElementById('app'));
-
-
-var homedir = require('os').homedir();
-
-var sauf = path.join(homedir, 'Documents', 'GTA San Andreas User Files');
-
-
-var fm = new FileManager(sauf);
-
-console.log(fm.getFiles());
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import path from 'path';
 import SaveList from './App/SaveList';
+
 
 export default class App extends React.Component {
     render() {
+        let homedir = require('os').homedir();
+        let sauf = path.join(homedir, 'Documents', 'GTA San Andreas User Files');
+
         return (
             <div>
-                <SaveList path="C:\\Users\\Jamie\\Documents\\GTA San Andreas User Files" />
+                <SaveList path={sauf} />
             </div>
         )
     }
