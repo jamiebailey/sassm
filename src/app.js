07c87c25c003721751cd218c3ef30d1ee295471b
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-05 22:27:58 +0000

    added save component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 // load stylesheet
 import '../assets/app.scss';
 
-// load app
+// load app libraries
 import path from 'path';
 import FileManager from './object/FileManager';
+
+// setup react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Save from './components/Save';
+
+ReactDOM.render(<Save />, window.document.querySelector('#app'));
+
 
 var homedir = require('os').homedir();
 
---
+++ src/components/Save.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Save extends React.Component {
+    render() {
+        return (
+            <h1>Save File</h1>
+        )
+    }
+}
