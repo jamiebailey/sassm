df021154d4c9f289e239c31c5119f0816a6657f6
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-05 22:19:48 +0000

    extracted versions

---
+++ src/constants/versions.js
@@ -0,0 +1,5 @@
+export default {
+    0x7581DA35: '1.00 Unmodified EXE',
+    0x83E5F365: '1.00 Modified EXE',
+    0x5E764593: '1.01 Modified EXE'
+};
--- src/object/SaveFile.js
+++ src/object/SaveFile.js
@@ -1,11 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-
-const VERSIONS = {
-    0x7581DA35: '1.00 Unmodified EXE',
-    0x83E5F365: '1.00 Modified EXE',
-    0x5E764593: '1.01 Modified EXE'
-};
+import VERSIONS from '../constants/versions';
 
 module.exports = class SaveFile {
     constructor(filepath) {
