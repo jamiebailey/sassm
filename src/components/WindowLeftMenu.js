bc5ee1c3c67f10fcf8c5dcf603df38d13d012f6c
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-06 20:33:34 +0000

    close button working

--- src/components/WindowLeftMenu.js
+++ src/components/WindowLeftMenu.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-
+import { remote } from 'electron';
 
 export default class WindowLeftMenu extends Component {
+    close() {
+        var window = remote.BrowserWindow.getFocusedWindow();
+        window.close();
+    }
     render() {
         return (
             <div className="WindowLeftMenu">
                 <a>Active</a>
                 <a>Storage</a>
                 <a>Settings</a>
-                <a>Exit</a>
+                <a onClick={this.close}>Exit</a>
             </div>
         );
     }
