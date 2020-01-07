3eb99dc994e74d5826148f9f57af309ab6e2e41c
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-07 00:19:29 +0000

    added default dir

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
+import path from 'path';
+import { setActiveDir, toggleActiveTab } from './actions';
+
+// Set default dir
+let defaultDir = path.resolve(require('os').homedir(), './Documents/GTA San Andreas User Files');
+
+store.dispatch(setActiveDir(defaultDir));
+store.dispatch(toggleActiveTab(true));
 
 render(
     <Provider store={store}>
