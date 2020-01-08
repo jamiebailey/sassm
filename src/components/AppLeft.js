bdb2e60035c544f397ca38f1735fba097e2e0e4e
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-08 23:07:19 +0000

    fixed Appleft

--- src/components/AppLeft.js
+++ src/components/AppLeft.js
@@ -3,7 +3,7 @@ import Button from './elements/Button';
 
 export default class AppLeft extends Component {
     render() {
-        const items = this.props.items.map((item) => <li><Button onClick={item.onClick}>{item.text}</Button></li>);
+        const items = this.props.items.map((item) => <li key={item.text}><Button onClick={item.onClick}>{item.text}</Button></li>);
         return <div className="app-left"><ul>{items}</ul></div>
     }
 }
